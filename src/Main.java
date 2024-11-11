5725598c9d819de81a63f487f7a6b213725a0b7a
SOVA <sova16tais@gmail.com>
2024-11-11 16:24:56 +0300

    Did homework.Part 2

--- src/Main.java
+++ src/Main.java
@@ -97,6 +97,85 @@ public class Main {
                 System.out.println("Число three больше остальных");
             }
         }
+
+        //Homework.Part 2
+        //Task 1
+
+        int clientOS=0;
+        if (clientOS==0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS==1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }else {
+            System.out.println("Ошибка");
+        }
+
+        //Task 2
+
+        clientOS=0;
+        int clientDeviceYea=2013;
+        if (clientDeviceYea<=2015&&clientOS==0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientDeviceYea>2015&&clientOS==0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientDeviceYea<=2015&&clientOS==1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientDeviceYea>2015&&clientOS==1) {
+            System.out.println("Установите обычную версию приложения для Android по ссылке");
+        }else {
+            System.out.println("Ошибка данных");
+        }
+
+        //Task 3
+
+        int year=2024;
+        if (year>1584 && ((year %4==0 && year %100!=0)||year %400==0)) {
+            System.out.println(year+" "+"год является високосным");
+        }else {
+            System.out.println(year+" "+"год не является високосным");
+        }
+
+        //Task 4
+
+        int deliveryDistance=95;
+        int days=1;
+        if (deliveryDistance<=20) {
+            System.out.println("Потребуется" + " " + days +" " + "дня для доставки карты");
+        } else if (deliveryDistance>20 && deliveryDistance<=60) {
+            System.out.println("Потребуется" + " " + days*2 +" " + "дня для доставки карты");
+        } else if (deliveryDistance>60 && deliveryDistance<=100) {
+            System.out.println("Потребуется" + " " + days*3 +" " + "дня для доставки карты");
+        }else{
+            System.out.println("Доставки нет");
+        }
+
+        //Task 5
+
+        int monthNumber=12;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Неверные данные");
+        }
     }
 }
 
